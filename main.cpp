2d44aaa427344fcc00f35b308d72fd878a5fb35f
jisookim <jisookimwork@gmail.com>
2024-05-13 09:25:10 +0900

    commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main() 
+{
+	int	Data = 0;
+	int Data2 = 0;
+
+	std::cin >> Data;
+	std::cin >> Data2;
+
+	std::cout << Data + Data2;
+	std::cout << Data - Data2;
+	std::cout << Data / Data2;
+	std::cout << Data * Data2;
+
+	return	0; 
+}
